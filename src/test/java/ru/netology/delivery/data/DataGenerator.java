3c7eb328841d56b731df80a13fb31d2cf8b8d135
valo <valorapaulinio@gmail.com>
2023-02-28 15:21:30 +0300

    add test

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -3,6 +3,7 @@ package ru.netology.delivery.data;
 import com.github.javafaker.Faker;
 import lombok.Value;
 import lombok.val;
+import org.jetbrains.annotations.Unmodifiable;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -16,24 +17,31 @@ public class DataGenerator {
     public static String generateDate(int shift) {
         // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
         // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         return date;
     }
 
     public static String generateCity(String locale) {
         // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
         // с помощью Faker, либо используя массив валидных городов и класс Random
+        Faker faker = new Faker(new Locale(locale));
+        String city = faker.address().city();
         return city;
     }
 
     public static String generateName(String locale) {
         // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
         // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
         return name;
     }
 
     public static String generatePhone(String locale) {
         // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
         // использовать Faker
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
         return phone;
     }
 
@@ -41,9 +49,13 @@ public class DataGenerator {
         private Registration() {
         }
 
-        public static UserInfo generateUser(String locale) {
+        public static  String generateUser(String locale) {
             // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
             // generateName(locale), generatePhone(locale)
+            Faker faker = new Faker(new Locale(locale));
+           String user =generateCity(locale);
+           user = generateName(locale);
+           user = generatePhone(locale);
             return user;
         }
     }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -3,9 +3,10 @@ package ru.netology.delivery.test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 class DeliveryTest {
 
@@ -17,6 +18,17 @@ class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
+        $("input[type='text']").setValue(DataGenerator.generateCity("ru"));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("input[type='tel']").setValue(DataGenerator.generateDate(4));
+        $("input[name='name']").setValue(DataGenerator.generateName("ru"));
+        $("input[name='phone']").setValue(DataGenerator.generatePhone("ru"));
+        $("[data-test-id='agreement']").click();
+        $x("//span[text()='Запланировать']").click();
+        $x("//span[text()='Запланировать']").click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("input[type='tel']").setValue(DataGenerator.generateDate(7));
+        $x("//span[text()='Перепланировать']").click();
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
